d522f9cdd3ec7e015500d421e791331c56489cd9
Quang-Linh LE <linktohack@gmail.com>
2015-12-02 04:02:16 +0100

    New implementation

--- ox-latex-subfigure.el
+++ ox-latex-subfigure.el
@@ -18,43 +18,30 @@
           (link/latex-table-to-subfigure limit)
           (buffer-string))))))
 
-
 (defun link/latex-table-to-subfigure (limit)
   "Convert well-formed table to subfigure."
   (interactive "p")
   (let ((width ".9\\textwidth")
         (align "b")
         (option "")
-        (count 0)
-        (centeringp nil)
-        (endp nil)
-        (template "\\begin{subfigure}[%s]{%s}
-%s
-\\caption{%s}
-\\end{subfigure}\n"))
-    (while (not endp)
-      (beginning-of-line)
-      (cond
-       ;; \begin{table}[option], \end{table}
-       ((looking-at "^\\\\\\(begin\\|end\\){table}")
-        (let ((row (thing-at-point 'line t))
-              (begend ""))
-          (string-match "^\\\\\\(begin\\|end\\){table}\\(\\[.*\\]\\)?" row)
-          (setq begend (match-string 1 row))
-          (setq option (or (match-string 2 row) ""))
-          (setq endp (string= "end" begend))
-          (kill-whole-line)
-          (insert (format "\\%s{figure}%s\n" begend option))))
-       ;; \centering
-       ((looking-at "^\\\\centering")
-        (setq centeringp t)
-        (next-line))
-       ;; \begin{subfigure}{width}{align}, \begin{subfigure}{align}
-       ;; \end{subfigure}
-       ((looking-at "^\\\\\\(begin\\|end\\){subfigure}")
-        (let ((row (thing-at-point 'line t))
-              maybe-width maybe-align)
-          (when (string-match "{subfigure}\\({\\(.*?\\)}\\)?\\({\\(.*?\\)}\\)?" row)
+        (centering "")
+        (caption "")
+        fig cap)
+    (beginning-of-line)
+    (while (not (looking-at "^\\\\end{table}"))
+      (let ((row (thing-at-point 'line t)))
+        (kill-whole-line)
+        (cond
+         ;; \begin{table}[option], \end{table}
+         ((string-match "^\\\\begin{table}\\(\\[.*\\]\\)?" row)
+          (setq option (or (match-string 1 row) "")))
+         ;; \centering
+         ((string-match "^\\\\centering" row)
+          (setq centering "\\centering\n"))
+         ;; \begin{subfigure}{width}{align}, \begin{subfigure}{align}
+         ;; \end{subfigure}
+         ((string-match "^\\\\begin{subfigure}\\({\\(.*?\\)}\\)?\\({\\(.*?\\)}\\)?" row)
+          (let (maybe-width maybe-align)
             (setq maybe-width (match-string 2 row))
             (setq maybe-align (match-string 4 row))
             (unless maybe-align
@@ -62,41 +49,37 @@
               (setq maybe-width nil))
             (setq width (or maybe-width width))
             (setq align (or maybe-align align))))
-        (kill-whole-line))
-       ;; \toprule, \midrule, \bottomrule
-       ((looking-at "^\\\\\\(top\\|mid\\|bottom\\)rule")
-        (kill-whole-line))
-       ;; table row
-       ((looking-at ".*\\\\\\\\$")
-        (let ((row1 nil)
-              (row2 nil)
-              (cell1 nil)
-              (cell2 nil))
-          (setq row1 (thing-at-point 'line t))
-          (kill-whole-line)
-          (setq row2 (thing-at-point 'line t))
-          (kill-whole-line)
-          (setq row1 (replace-regexp-in-string "\\\\\\\\\n$" "" row1))
-          (setq row2 (replace-regexp-in-string "\\\\\\\\\n$" "" row2))
-          (setq cell1 (split-string row1 " & "))
-          (setq cell2 (split-string row2 " & "))
-          (dolist (cell (cl-mapcar #'cons cell1 cell2))
-            (let ((fig (car cell))
-                  (cap (cdr cell)))
-              (setq count (1+ count))
-              (when (and (> limit 0)
-                         (> count limit))
-                (setq count 1)
-                (insert (format "\\end{figure}\n
+         ;; \caption{\label{tab:orgtable1}
+         ;; xxx}
+         ((string-match "^\\\\caption{" row)
+          (setq caption (concat row (thing-at-point 'line t)))
+          (kill-whole-line))
+         ;; table row
+         ((string-match ".*\\\\\\\\$" row)
+          (let ((striped-row (replace-regexp-in-string "\\\\\\\\\n$" "" row)))
+            (setq fig (append (split-string striped-row " & ") fig))
+            (setq row (thing-at-point 'line t))
+            (kill-whole-line)
+            (setq striped-row (replace-regexp-in-string "\\\\\\\\\n$" "" row))
+            (setq cap (append (split-string striped-row " & ") cap)))))))
+    (kill-whole-line)
+    (insert (format "\\begin{figure}%s\n%s" option centering))
+    (dotimes (i (length fig))
+      (let ((f (nth i fig))
+            (c (nth i cap)))
+        (when (string-match "includegraphics" c)
+          (setq f (prog1 c (setq c f))))
+        (when (string-match "includegraphics" f)
+          (insert (format "\\begin{subfigure}[%s]{%s}
+%s
+\\caption{%s}
+\\end{subfigure}\n" align width f c))
+          (when (and (> limit 0)
+                     (< i (1- (length fig)))
+                     (= (mod (1+ i) limit) 0))
+            (insert (format "\\end{figure}\n
 \\begin{figure}%s
-\\ContinuedFloat\n" option))
-                (when centeringp
-                  (insert "\\centering\n")))
-              (if (string-match "includegraphics" fig)
-                  (insert (format template align width fig cap))
-                (if (string-match "includegraphics" cap)
-                    (insert (format template align width cap fig))))))))
-       ;; skip other
-       (t (next-line))))))
+%s\\ContinuedFloat\n" option centering))))))
+    (insert (format "%s\\end{figure}\n" caption))))
 
 (provide 'ox-latex-subfigure)
